0de6ff97024d614bdfe26ba1e6eb81b469540951
Tuan Nguyen <tuannguyenhoangit@gmail.com>
2018-09-11 20:39:21 +0700

    Update import path

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import 'react-table/react-table.css'
 import Purchase from './components/purchase'
-import AddPurchase from './components/addpurchase'
+import AddPurchase from './components/AddPurchase'
 import Print from './components/print'
-import PrintDetail from './components/printdetail'
-import PurchaseList from './components/purchaselist'
+import PrintDetail from './components/PrintDetail'
+import PurchaseList from './components/PurchaseList'
 import {
   BrowserRouter as Router,
   Route,
@@ -17,20 +17,20 @@ import {
 } from "react-router-dom";
 
 class App extends Component {
-   render() {
-      return (
-          <div className="App">
-              <Router>
-                <Switch>
-                  <Route exact path="/" component={PurchaseList} />
-                  <Route exact path="/baogia/:purchaseId" component={Print} />
-                  <Route exact path="/chitiethopdong/:purchaseId" component={PrintDetail} />
-                  <Route exact path="/main" component={AddPurchase} />
-                  <Route exact path="/purchase/:purchaseId" component={Purchase} />
-                </Switch>
-              </Router>
-          </div>
-      );
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={PurchaseList} />
+            <Route exact path="/baogia/:purchaseId" component={Print} />
+            <Route exact path="/chitiethopdong/:purchaseId" component={PrintDetail} />
+            <Route exact path="/main" component={AddPurchase} />
+            <Route exact path="/purchase/:purchaseId" component={Purchase} />
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
 
